66ce72f88f76c9b8a5591d6236a26a548239bf1c
DryWall <978duncan2@gmail.com>
2024-05-18 12:58:39 -0400

    huh

--- DungeonPort/obj/Debug/netcoreapp3.1/DungeonPort.AssemblyInfo.cs
+++ DungeonPort/obj/Debug/netcoreapp3.1/DungeonPort.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonPort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26da3bd88cfa8a23e0aacdb6ae00ce4eb0f3ec72")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonPort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonPort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
